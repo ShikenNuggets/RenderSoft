4dd333c4615f8351657f798f82f4cd2bbb613e23
ShikenNuggets <carter.rennick@gmail.com>
2026-01-02 12:43:14 -0500

    First software rendering experiment: manually draw red pixels

--- src/RenderSoft.cpp
+++ src/RenderSoft.cpp
@@ -8,14 +8,32 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello, World!" << std::endl;
 
-	auto window = new Gadget::Window(800, 600, Gadget::RenderAPI::None);
-	int count = 0;
-	while (count < 5000000)
+	//auto window = new Gadget::Window(800, 600, Gadget::RenderAPI::None);
+	//delete window;
+
+	SDL_Window* window = SDL_CreateWindow("Software Rendering Surface", 800, 600, 0);
+	SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+
+	SDL_LockSurface(screenSurface);
+
+	const auto pixels = static_cast<uint32_t*>(screenSurface->pixels);
+	const auto width = screenSurface->w;
+	const auto height = screenSurface->h;
+	const auto pitchInPixels = screenSurface->pitch / sizeof(screenSurface->pitch);
+	const auto redColor = SDL_MapSurfaceRGB(screenSurface, 255, 0, 0);
+
+	for (int y = 0; y < height; y++)
 	{
-		window->HandleEvents();
-		count++;
+		for (int x = 0; x < width; x++)
+		{
+			pixels[y * pitchInPixels + x] = redColor;
+		}
 	}
 
-	delete window;
+	SDL_UnlockSurface(screenSurface);
+	SDL_UpdateWindowSurface(window);
+
+	std::cin.get();
+	SDL_DestroyWindow(window);
 	return 0;
 }
