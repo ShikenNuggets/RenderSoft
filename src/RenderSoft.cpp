fadff5d6517b352f805fd9595586cf513fd05caf
ShikenNuggets <carter.rennick@gmail.com>
2026-01-04 20:35:24 -0500

    Improvements

--- src/RenderSoft.cpp
+++ src/RenderSoft.cpp
@@ -9,7 +9,7 @@
 #include "ImageView.hpp"
 #include "Mesh.hpp"
 
-static inline double Det2D(const Gadget::Vector4 & v0, const Gadget::Vector4 & v1)
+static inline double Det2D(const Gadget::Vector3& v0, const Gadget::Vector3& v1)
 {
 	return v0.x * v1.y - v0.y * v1.x;
 }
@@ -18,19 +18,15 @@ static void Draw(RS::ImageView& imageView, const RS::Mesh& mesh)
 {
 	for (size_t v = 0; v + 2 < mesh.vertices.size(); v += 3)
 	{
-		const auto& pos0 = mesh.vertices[v].position;
-		const auto& pos1 = mesh.vertices[v + 1].position;
-		const auto& pos2 = mesh.vertices[v + 2].position;
-
-		auto v0 = Gadget::Vector4(pos0.x, pos0.y, pos0.z);
-		auto v1 = Gadget::Vector4(pos1.x, pos1.y, pos1.z);
-		auto v2 = Gadget::Vector4(pos2.x, pos2.y, pos2.z);
+		const auto& v0 = mesh.vertices[v].position;
+		const auto& v1 = mesh.vertices[v + 1].position;
+		const auto& v2 = mesh.vertices[v + 2].position;
 
 		for (int y = 0; y < imageView.Height(); y++)
 		{
 			for (int x = 0; x < imageView.Width(); x++)
 			{
-				auto p = Gadget::Vector4(x + 0.5f, y + 0.5f, 0.0f, 0.0f);
+				auto p = Gadget::Vector3(x + 0.5f, y + 0.5f, 0.0f);
 
 				auto det01p = Det2D(v1 - v0, p - v0);
 				auto det12p = Det2D(v2 - v1, p - v1);
@@ -47,12 +43,15 @@ static void Draw(RS::ImageView& imageView, const RS::Mesh& mesh)
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
+	static constexpr auto screenW = 800;
+	static constexpr auto screenH = 600;
+
 	std::println("Hello, World!");
 
 	//auto window = new Gadget::Window(800, 600, Gadget::RenderAPI::None);
 	//delete window;
 
-	SDL_Window* window = SDL_CreateWindow("Software Rendering Surface", 800, 600, 0);
+	SDL_Window* window = SDL_CreateWindow("Software Rendering Surface", screenW, screenH, 0);
 	SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
 
 	RS::FrameCounter counter;
@@ -64,9 +63,9 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 
 	auto triMesh = RS::Mesh();
 	triMesh.vertices.reserve(3);
-	triMesh.vertices.emplace_back(Gadget::Vector3(100, 100, 0.0));
-	triMesh.vertices.emplace_back(Gadget::Vector3(200, 100, 0.0));
-	triMesh.vertices.emplace_back(Gadget::Vector3(100, 200, 0.0));
+	triMesh.vertices.emplace_back(Gadget::Vector3(400.0, 100.0, 0.0));
+	triMesh.vertices.emplace_back(Gadget::Vector3(600.0, 500.0, 0.0));
+	triMesh.vertices.emplace_back(Gadget::Vector3(200.0, 500.0, 0.0));
 	triMesh.color = Gadget::Vector4(1.0, 0.0, 0.0, 1.0);
 
 	while (true)
@@ -80,16 +79,18 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 		curTime = std::chrono::system_clock::now().time_since_epoch();
 		counter.AddFrameTime(std::chrono::duration_cast<std::chrono::microseconds>(curTime - prevTime));
 
-		auto currentTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+		//auto currentTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
 		imageView.Lock();
 
-		imageView.Clear(Gadget::Vector4(
+		/*imageView.Clear(Gadget::Vector4(
 			std::sin(static_cast<double>(currentTimeMs) / 1000.0),
 			std::sin(static_cast<double>(currentTimeMs) / 1000.0 - 1),
 			std::sin(static_cast<double>(currentTimeMs) / 1000.0 - 2),
 			1.0
-		));
+		));*/
+
+		imageView.Clear(Gadget::Vector4(0.1, 0.1, 0.1, 1.0));
 
 		Draw(imageView, triMesh);
 
