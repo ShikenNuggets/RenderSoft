7cb7fb80ed0947eb887c134521e0c5e7c7183871
ShikenNuggets <carter.rennick@gmail.com>
2026-01-12 21:24:37 -0500

    sRGB gamma correction

--- src/ImageView.cpp
+++ src/ImageView.cpp
@@ -59,7 +59,36 @@ void ImageView::AssignPixel(int32_t x, int32_t y, Uint32 color)
 
 void ImageView::AssignPixel(int32_t x, int32_t y, const Gadget::Vector4& color)
 {
-	const auto finalColor = SDL_MapSurfaceRGB(surface, color.x * 255, color.y * 255, color.z * 255);
+	Gadget::Vector4 sRGB;
+
+	if (color.x < 0.00313066844250063)
+	{
+		sRGB.x = color.x * 12.92;
+	}
+	else
+	{
+		sRGB.x = 1.055 * (std::pow(color.x, 1.0 / 2.4)) - 0.055;
+	}
+
+	if (color.y < 0.00313066844250063)
+	{
+		sRGB.y = color.y * 12.92;
+	}
+	else
+	{
+		sRGB.y = 1.055 * (std::pow(color.y, 1.0 / 2.4)) - 0.055;
+	}
+
+	if (color.z < 0.00313066844250063)
+	{
+		sRGB.z = color.z * 12.92;
+	}
+	else
+	{
+		sRGB.z = 1.055 * (std::pow(color.z, 1.0 / 2.4)) - 0.055;
+	}
+
+	const auto finalColor = SDL_MapSurfaceRGB(surface, sRGB.x * 255, sRGB.y * 255, sRGB.z * 255);
 	AssignPixel(x, y, finalColor);
 }
 
