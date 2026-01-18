78e262b37ec828e7c9f36f4e0ae1831f4b354f4c
ShikenNuggets <carter.rennick@gmail.com>
2026-01-18 16:38:54 -0500

    Cube rendering

--- src/RenderSoft.cpp
+++ src/RenderSoft.cpp
@@ -111,27 +111,134 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 
 	auto viewport = RS::Viewport(0, screenW, 0, screenH);
 
-	auto triMesh = RS::Mesh();
-	triMesh.vertices.reserve(3);
-	triMesh.vertices.emplace_back(Gadget::Vector3(-0.5, -0.5, 0.0));
-	triMesh.vertices.emplace_back(Gadget::Vector3(-0.5, 0.5, 0.0));
-	triMesh.vertices.emplace_back(Gadget::Vector3(0.5, -0.5, 0.0));
-	triMesh.vertices.emplace_back(Gadget::Vector3(0.5, 0.5, 0.0));
-	//triMesh.vertices.emplace_back(Gadget::Vector3(0.0, 0.0, 0.0));
-	//triMesh.vertices.emplace_back(Gadget::Vector3(0.0, 100.0, 0.0));
-	//triMesh.vertices.emplace_back(Gadget::Vector3(100.0, 0.0, 0.0));
+	auto rectMesh = RS::Mesh();
+	rectMesh.vertices.reserve(4);
+	rectMesh.vertices.emplace_back(Gadget::Vector3(-0.5, -0.5, 0.0));
+	rectMesh.vertices.emplace_back(Gadget::Vector3(-0.5, 0.5, 0.0));
+	rectMesh.vertices.emplace_back(Gadget::Vector3(0.5, -0.5, 0.0));
+	rectMesh.vertices.emplace_back(Gadget::Vector3(0.5, 0.5, 0.0));
 
-	triMesh.indices.push_back(0);
-	triMesh.indices.push_back(1);
-	triMesh.indices.push_back(2);
+	rectMesh.indices.push_back(0);
+	rectMesh.indices.push_back(1);
+	rectMesh.indices.push_back(2);
 
-	triMesh.indices.push_back(2);
-	triMesh.indices.push_back(1);
-	triMesh.indices.push_back(3);
+	rectMesh.indices.push_back(2);
+	rectMesh.indices.push_back(1);
+	rectMesh.indices.push_back(3);
 
-	triMesh.vertices[0].color = Gadget::Vector4(1.0, 0.0, 0.0, 1.0);
-	triMesh.vertices[1].color = Gadget::Vector4(0.0, 1.0, 0.0, 1.0);
-	triMesh.vertices[2].color = Gadget::Vector4(0.0, 0.0, 1.0, 1.0);
+	rectMesh.vertices[0].color = Gadget::Vector4(1.0, 0.0, 0.0, 1.0);
+	rectMesh.vertices[1].color = Gadget::Vector4(0.0, 1.0, 0.0, 1.0);
+	rectMesh.vertices[2].color = Gadget::Vector4(0.0, 0.0, 1.0, 1.0);
+	rectMesh.vertices[3].color = Gadget::Vector4(0.0, 0.0, 0.0, 1.0);
+
+
+	auto cubeMesh = RS::Mesh();
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.0, -1.0, -1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, 1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, -1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, 1.f, 1.f));
+
+	// +X face
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, -1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, 1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, -1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, 1.f, 1.f));
+
+	// -Y face
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, -1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, -1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, -1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, -1.f, 1.f));
+
+	// +Y face
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, 1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, 1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, 1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, 1.f, 1.f));
+
+	// -Z face
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, -1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, -1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, 1.f, -1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, 1.f, -1.f));
+
+	// +Z face
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, -1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, -1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.f, 1.f, 1.f));
+	cubeMesh.vertices.push_back(Gadget::Vector3(1.f, 1.f, 1.f));
+
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.0, -1.0, -1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.0, -1.0,  1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.0,  1.0, -1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3(-1.0,  1.0,  1.0));
+
+	cubeMesh.vertices.push_back(Gadget::Vector3( 1.0, -1.0, -1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3( 1.0, -1.0,  1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3( 1.0,  1.0, -1.0));
+	cubeMesh.vertices.push_back(Gadget::Vector3( 1.0,  1.0,  1.0));
+
+	cubeMesh.indices.push_back(0); cubeMesh.indices.push_back(2); cubeMesh.indices.push_back(1);
+	cubeMesh.indices.push_back(1); cubeMesh.indices.push_back(2); cubeMesh.indices.push_back(3);
+
+	cubeMesh.indices.push_back(4); cubeMesh.indices.push_back(5); cubeMesh.indices.push_back(6);
+	cubeMesh.indices.push_back(6); cubeMesh.indices.push_back(5); cubeMesh.indices.push_back(7);
+
+	cubeMesh.indices.push_back(8); cubeMesh.indices.push_back(9); cubeMesh.indices.push_back(10);
+	cubeMesh.indices.push_back(10); cubeMesh.indices.push_back(9); cubeMesh.indices.push_back(11);
+
+	cubeMesh.indices.push_back(12); cubeMesh.indices.push_back(14); cubeMesh.indices.push_back(13);
+	cubeMesh.indices.push_back(14); cubeMesh.indices.push_back(15); cubeMesh.indices.push_back(13);
+
+	cubeMesh.indices.push_back(16); cubeMesh.indices.push_back(18); cubeMesh.indices.push_back(17);
+	cubeMesh.indices.push_back(17); cubeMesh.indices.push_back(18); cubeMesh.indices.push_back(19);
+
+	cubeMesh.indices.push_back(20); cubeMesh.indices.push_back(21); cubeMesh.indices.push_back(22);
+	cubeMesh.indices.push_back(21); cubeMesh.indices.push_back(23); cubeMesh.indices.push_back(22);
+
+
+	cubeMesh.vertices[0].color = Gadget::Vector4(0.f, 1.f, 1.f, 1.f);
+	cubeMesh.vertices[1].color = Gadget::Vector4(0.f, 1.f, 1.f, 1.f);
+	cubeMesh.vertices[2].color = Gadget::Vector4(0.f, 1.f, 1.f, 1.f);
+	cubeMesh.vertices[3].color = Gadget::Vector4(0.f, 1.f, 1.f, 1.f);
+
+	// +X face
+	cubeMesh.vertices[4].color = Gadget::Vector4(1.f, 0.f, 0.f, 1.f);
+	cubeMesh.vertices[5].color = Gadget::Vector4(1.f, 0.f, 0.f, 1.f);
+	cubeMesh.vertices[6].color = Gadget::Vector4(1.f, 0.f, 0.f, 1.f);
+	cubeMesh.vertices[7].color = Gadget::Vector4(1.f, 0.f, 0.f, 1.f);
+
+	// -Y face
+	cubeMesh.vertices[8].color = Gadget::Vector4(1.f, 0.f, 1.f, 1.f);
+	cubeMesh.vertices[9].color = Gadget::Vector4(1.f, 0.f, 1.f, 1.f);
+	cubeMesh.vertices[10].color = Gadget::Vector4(1.f, 0.f, 1.f, 1.f);
+	cubeMesh.vertices[11].color = Gadget::Vector4(1.f, 0.f, 1.f, 1.f);
+
+	// +Y face
+	cubeMesh.vertices[12].color = Gadget::Vector4(0.f, 1.f, 0.f, 1.f);
+	cubeMesh.vertices[13].color = Gadget::Vector4(0.f, 1.f, 0.f, 1.f);
+	cubeMesh.vertices[14].color = Gadget::Vector4(0.f, 1.f, 0.f, 1.f);
+	cubeMesh.vertices[15].color = Gadget::Vector4(0.f, 1.f, 0.f, 1.f);
+
+	// -Z face
+	cubeMesh.vertices[16].color = Gadget::Vector4(1.f, 1.f, 0.f, 1.f);
+	cubeMesh.vertices[17].color = Gadget::Vector4(1.f, 1.f, 0.f, 1.f);
+	cubeMesh.vertices[18].color = Gadget::Vector4(1.f, 1.f, 0.f, 1.f);
+	cubeMesh.vertices[19].color = Gadget::Vector4(1.f, 1.f, 0.f, 1.f);
+
+	// +Z face
+	cubeMesh.vertices[20].color = Gadget::Vector4(0.f, 0.f, 1.f, 1.f);
+	cubeMesh.vertices[21].color = Gadget::Vector4(0.f, 0.f, 1.f, 1.f);
+	cubeMesh.vertices[22].color = Gadget::Vector4(0.f, 0.f, 1.f, 1.f);
+	cubeMesh.vertices[23].color = Gadget::Vector4(0.f, 0.f, 1.f, 1.f);
+
+	auto transform = Gadget::Matrix4::Identity();
+	transform *= Gadget::Math::Scale(Gadget::Vector3(0.5, 0.5, 0.5));
+
+	// Scale by aspect ratio
+	// TODO - This is a hack that we'll address with projection matrices later
+	auto aspect = screenH * (1.0 / screenW);
+	transform *= Gadget::Math::Scale(Gadget::Vector3(aspect, 1.0, 1.0));
 
 	while (true)
 	{
@@ -143,6 +250,10 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 
 		curTime = std::chrono::system_clock::now().time_since_epoch();
 		counter.AddFrameTime(std::chrono::duration_cast<std::chrono::microseconds>(curTime - prevTime));
+
+		double deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(curTime - prevTime).count() / 1000.0;
+		transform *= Gadget::Math::Rotate(deltaTime * 25.0, Gadget::Vector3(0.0, 1.0, 0.0));
+		transform *= Gadget::Math::Rotate(deltaTime * 25.0 * 1.5, Gadget::Vector3(1.0, 0.0, 0.0));
 
 		//auto currentTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
@@ -157,7 +268,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 
 		imageView.Clear(Gadget::Color(0.1, 0.1, 0.1));
 
-		Draw(viewport, imageView, RS::DrawCall(triMesh));
+		Draw(viewport, imageView, RS::DrawCall(cubeMesh, transform));
 		//Draw(viewport, imageView, RS::DrawCall(triMesh, Gadget::Math::Translate(Gadget::Vector3(-150.0, -50.0, 0.0))));
 		//Draw(viewport, imageView, RS::DrawCall(triMesh, Gadget::Math::Translate(Gadget::Vector3(150.0, 175.0, 0.0))));
 		//for (int i = 0; i < 64; i++)
